6c004973b37f48d9273084d1d3993b7474ebc0c0
amir77hm <amir77hm@gmail.com>
2021-01-14 16:21:18 +0330

    first-version

--- controllers/tasksController.js
+++ controllers/tasksController.js
@@ -16,8 +16,8 @@ exports.store = (req, res) => {
 
         if (error) throw error
 
-        res.redirect('/tasks/add')
     })
+    res.redirect('/tasks/add')
 }
 
 exports.remove = (req, res) => {
@@ -28,11 +28,47 @@ exports.remove = (req, res) => {
 }
 
 exports.edit = (req, res) => {
-    // db.query('UPDATE tasks SET task_title = ?, task_category = ?, task_assignee = ? , task_status = ? WHERE id = ?', ['a', 'b', 'c', req.params.id], function (error, results, fields) {
-    //     if (error) throw error;
-    //     // ...
-    // })
-    // console.log(req.body.task_title)
-    res.render('tasks/edit')
 
+    const urlParams = parseURLParams(req.url)
+    if (urlParams !== undefined) {
+
+        let task_status;
+        switch (urlParams.inlineRadioOptions[0]) {
+            case 'option1':
+                task_status = 1
+                break;
+            case 'option2':
+                task_status = 2
+                break;
+        }
+
+        db.query('UPDATE tasks SET task_title = ?, task_category = ?, task_assignee = ?, task_status =?  WHERE task_id = ?', [`${urlParams.task_title}`, `${urlParams.task_category}`, `${urlParams.task_assignee}`, task_status, req.params.id], function (error, results, fields) {
+            if (error) throw error;
+            console.log(urlParams)
+        });
+    }
+
+
+    res.render('tasks/edit')
+}
+
+// get value from url
+function parseURLParams(url) {
+    var queryStart = url.indexOf("?") + 1,
+        queryEnd = url.indexOf("#") + 1 || url.length + 1,
+        query = url.slice(queryStart, queryEnd - 1),
+        pairs = query.replace(/\+/g, " ").split("&"),
+        parms = {}, i, n, v, nv;
+
+    if (query === url || query === "") return;
+
+    for (i = 0; i < pairs.length; i++) {
+        nv = pairs[i].split("=", 2);
+        n = decodeURIComponent(nv[0]);
+        v = decodeURIComponent(nv[1]);
+
+        if (!parms.hasOwnProperty(n)) parms[n] = [];
+        parms[n].push(nv.length === 2 ? v : null);
+    }
+    return parms;
 }
